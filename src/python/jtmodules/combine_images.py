3e17c016ee170c4e47be568ca0b0dfa72bce738d
Markus D. Herrmann <HackerMD@users.noreply.github.com>
2016-07-13 18:19:59 +0200

    Update combine_images.py

--- src/python/jtmodules/combine_images.py
+++ src/python/jtmodules/combine_images.py
@@ -8,26 +8,26 @@ VERSION = '0.1.0'
 def combine_images(image01, image02, multiplication_factor01=1,
                    multiplication_factor02=1, plot=False):
     ''' Combine two grayscale images together. The output is a 16-bit grayscale
-        image. Note that each image can be multiplied by a factor (positive 
-        integer only) prior to combination. Default factor for both images is 1.
+    image. Note that each image can be multiplied by a factor (positive 
+    integer only) prior to combination. Default factor for both images is 1.
 
-        For arithmetics images are cast to type ``float``. This step is made to ensure
-        that the resulting intensities values can lie beyond the range of the
-        bit depth (>65536 in the case of a 16-bit image).
+    For arithmetics images are cast to type ``float``. This step is made to ensure
+    that the resulting intensities values can lie beyond the range of the
+    bit depth (>65536 in the case of a 16-bit image).
         
-        Each image is then individually multiplied by its respective
-        multiplication factor.
+    Each image is then individually multiplied by its respective
+    multiplication factor.
         
-        The images are then added and the combined image is divided by the sum
-        of the two multiplication factors. In consequence the range of the
-        combined image is the same as the original images.
+    The images are then added and the combined image is divided by the sum
+    of the two multiplication factors. In consequence the range of the
+    combined image is the same as the original images.
 
-        The combined image is finally cast back to its original data type.
+    The combined image is finally cast back to its original data type.
 
-        For example, the combine_images module could be used to combine the
-        image where lipid droplets were imaged and the image where the cell 
-        outline was imaged. The output image can be used as a mask to properly 
-        segment adipocytes.
+    For example, the combine_images module could be used to combine the
+    image where lipid droplets were imaged and the image where the cell 
+    outline was imaged. The output image can be used as a mask to properly 
+    segment adipocytes.
 
     Parameters
     ----------
@@ -58,6 +58,10 @@ def combine_images(image01, image02, multiplication_factor01=1,
     Author
     ------
     Christophe Freyre
+    
+    Note
+    ----
+    Images will be clipped at different values for visualization.
     '''
     #Error types
     if not isinstance(multiplication_factor01, int):
@@ -94,10 +98,9 @@ def combine_images(image01, image02, multiplication_factor01=1,
     #Plotting
     if plot:
         from jtlib import plotting
-        clip_val = np.percentile(image01, 99.9)
         plots = [
-            plotting.create_intensity_image_plot(image01, 'ul', clip=True, clip_val),
-            plotting.create_intensity_image_plot(image02, 'ur', clip=True, clip_val),
+            plotting.create_intensity_image_plot(image01, 'ul', clip=True),
+            plotting.create_intensity_image_plot(image02, 'ur', clip=True),
             plotting.create_intensity_image_plot(combined_image, 'll', clip=True),
         ]
         outputs['figure'] = plotting.create_figure(
