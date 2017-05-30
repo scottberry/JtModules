d25212b5e102db6f51ab8cbf3bc6a419be300f56
scottberry <scottdberry@gmail.com>
2017-05-30 08:18:28 +0000

    Renamed mask as apply_mask and generalised input to MaskImage

--- src/python/jtmodules/apply_mask.py
+++ src/python/jtmodules/apply_mask.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-'''Jterator module for plotting a LabelImage
+'''Jterator module for applying a mask to a label image
 '''
 import logging
 import collections
